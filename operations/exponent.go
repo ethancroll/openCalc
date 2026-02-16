636ff7bf4ca32dab7e0f3575990f423bcae7b9b1
ethan carroll <100100385+ethancroll@users.noreply.github.com>
2026-02-16 10:31:57 -0600

    added the exponent operation

---
+++ operations/exponent.go
@@ -0,0 +1,25 @@
+package operations
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+// takes the exponentiation expression, and returns the result
+func Exponent(expr string) (float64, error) {
+	parts := strings.Split(expr, "e") // ["8","9"]
+
+	a, err := strconv.ParseFloat(parts[0], 64) // left side of expression
+	if err != nil {
+		return 0, fmt.Errorf("'%s' is not a valid number", parts[0])
+	}
+
+	b, err := strconv.ParseFloat(parts[1], 64) // right side of expression
+	if err != nil {
+		return 0, fmt.Errorf("'%s' is not a valid number", parts[1])
+	}
+
+	return math.Pow(a, b), nil
+}
